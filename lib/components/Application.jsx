ffd5a43a0fdcd0551f6f0e25bc1b247b9085533f
gness1804 <graham.nessler@gmail.com>
2016-11-07 19:53:01 -0700

    initial commit

---
+++ lib/components/Application.jsx
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+
+const ReactDOM = require('react-dom');
+
+class Application extends Component {
+
+  render() {
+    return (
+      <div>Another sanity check.</div>
+    );
+  }
+
+}
+
+
+export default Application;
