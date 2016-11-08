96c11a7e94ae2e920d4a7943673d63a8c929fb58
gness1804 <graham.nessler@gmail.com>
2016-11-08 12:51:54 -0700

    finally got sanity check test to pass

--- test/index.js
+++ test/index.js
@@ -1,1 +1,8 @@
-const assert = require('chai');
+const assert = require('chai').assert;
+require('../lib/vanilla');
+
+describe('vanilla', () => {
+  it('sanity check', () => {
+    assert(true);
+  });
+});
