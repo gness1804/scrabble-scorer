d879a9909cf04b4d1dcbd64f6d83a8dd4e5caace
gness1804 <graham.nessler@gmail.com>
2016-11-08 19:13:50 -0700

    app is set up for assessment

--- lib/components/Application.jsx
+++ lib/components/Application.jsx
@@ -7,7 +7,7 @@ class Application extends Component {
 
   render() {
 
-    // console.log(scoreWord);
+    console.log(scoreWord('hello', 2)); //this works
 
     return (
       <div>Hello! This is a test. If I can see this, then React is set up correctly.</div>
