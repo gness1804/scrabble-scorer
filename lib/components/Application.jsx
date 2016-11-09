816111dd556fb2d9f40535d4783f34a568f1963b
gness1804 <graham.nessler@gmail.com>
2016-11-08 19:08:30 -0700

    scoreWord is successfully imported into Application

--- lib/components/Application.jsx
+++ lib/components/Application.jsx
@@ -1,10 +1,14 @@
 import React, {Component} from 'react';
+import scoreWord from '../vanilla.js';
 
 const ReactDOM = require('react-dom');
 
 class Application extends Component {
 
   render() {
+
+    // console.log(scoreWord);
+
     return (
       <div>Hello! This is a test. If I can see this, then React is set up correctly.</div>
     );
